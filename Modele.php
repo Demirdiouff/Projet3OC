0b0638691b68d43e49068f5e458ad54e221ce9b5
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-12-23 12:50:14 +0100

    Isolation connexion BDD

--- Modele.php
+++ Modele.php
@@ -2,7 +2,13 @@
 
 // Renvoi la liste de tous les billets, triés par identifiant décroissant
 function getBillets(){
+    $bdd = getBdd();
     $bdd = new PDO('mysql:host=localhost;dbname=Projet3OC;charset=utf8', 'root', 'root');
     $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
     return $billets;
+}
+
+function getBdd(){
+    $bdd = new PDO('mysql:host=localhost;dbname=Projet3OC;charset=utf8', 'root', 'root');
+    return $bdd;
 }
