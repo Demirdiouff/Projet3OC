c3d994bc88a13acb689fcb747ba7666f5b47318b
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-12-23 12:27:21 +0100

    Code accès aux données

---
+++ Modele.php
@@ -0,0 +1,8 @@
+<?php
+
+// Renvoi la liste de tous les billets, triés par identifiant décroissant
+function getBillets(){
+    $bdd = new PDO('mysql:host=localhost;dbname=Projet3OC;charset=utf8', 'root', 'root');
+    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
+    return $billets;
+}
