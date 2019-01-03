ef60138a3df466ab7d42a111b02ec87608c5f575
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-03 16:30:45 +0300

    Re-ecriture liens

--- Controleur/Controleur.php
+++ Controleur/Controleur.php
@@ -1,21 +1,21 @@
 <?php
 
-require 'Modele.php';
+require 'Modele/Modele.php';
 
 // Affiche la liste de tous les billets du blog
 function accueil(){
     $billets = getBillets();
-    require 'vueAccueil.php';
+    require 'Vue/vueAccueil.php';
 }
 
 // Affiche un billet et ses commentaires associés
 function billet($idBillet){
     $billet = getBillet($idBillet);
     $commentaires = getCommentaires($idBillet);
-    require 'vueBillet.php';
+    require 'Vue/vueBillet.php';
 }
 
 // Affiche les erreurs
 function erreur($msgErreur){
-    require 'vueErreurs.php';
+    require 'Vue/vueErreurs.php';
 }
