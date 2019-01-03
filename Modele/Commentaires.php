845cda2b88448a0bd4d6715976485cc5d71d3316
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-03 16:34:04 +0300

    Passage classe commentaire en Objet

--- Modele/Commentaires.php
+++ Modele/Commentaires.php
@@ -1,12 +1,18 @@
 <?php
 
-// Renvoie la liste des commentaires associés à un billet
-public function getCommentaires($idBillet)
+require_once 'Modele/Modele.php';
+
+class Commentaire extends Modele
 {
-    $bdd = $this->getBdd();
-    $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,' . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE' . ' where BIL_ID=?');
-    $commentaires->execute(array(
-        $idBillet
-    ));
-    return $commentaires;
+
+    // Renvoie la liste des commentaires associés à un billet
+    public function getCommentaires($idBillet)
+    {
+        $bdd = $this->getBdd();
+        $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,' . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE' . ' where BIL_ID=?');
+        $commentaires->execute(array(
+            $idBillet
+        ));
+        return $commentaires;
+    }
 }
