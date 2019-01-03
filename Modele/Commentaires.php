02ad8b983d6e160a4dc13ae12ae52b6c6034a5cc
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-03 16:32:14 +0300

    Ecriture classe commentaire en charge de l'accès aux données commentaires

---
+++ Modele/Commentaires.php
@@ -0,0 +1,12 @@
+<?php
+
+// Renvoie la liste des commentaires associés à un billet
+public function getCommentaires($idBillet)
+{
+    $bdd = $this->getBdd();
+    $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,' . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE' . ' where BIL_ID=?');
+    $commentaires->execute(array(
+        $idBillet
+    ));
+    return $commentaires;
+}
