014536cb666e7ba61ce385c83f3a82d58b762e12
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-11 19:11:52 +0100

    Correctif SQL et code

--- Modele/Billet.php
+++ Modele/Billet.php
@@ -15,9 +15,9 @@ class Billet extends Modele {
     // Renvoie les informations sur un billet
     public function getBillet($idBillet)
     {
-        $sql = ('select BIL_ID as id, BIL_DATE as date' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' where BIL_ID=?');
+        $sql = ('select BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' where BIL_ID=?');
         $billet = $this->executerRequete($sql, array($idBillet));
-        if ($billet->rowCount == 1) {
+        if ($billet->rowCount() == 1) {
             return $billet->fetch(); // Accès à la première ligne de résultat
         } else {
             throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
