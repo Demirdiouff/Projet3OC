12e22e0be57a04fb54e528e4fbcc9904a90e9434
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-12 19:07:12 +0100

    Ajout d'une vue Roman

---
+++ Vue/vueRoman.php
@@ -0,0 +1,18 @@
+<?php
+
+// Affichage des romans
+foreach ($billets as $billet): ?>
+	<article>
+		<header>
+			<a href="<?= "index.php?action=commentRoman&id=" . $billet['id'] ?>">
+				<h1 class="titreBillet"><?= $billet ['titre'] ?></h1>
+			</a>
+			<time><?= $billet['date'] ?></time>
+		</header>
+		<p><?= $billet['contenu'] ?></p>
+	</article>
+	<hr />
+ <?php endforeach; ?>
+ 
+ 
+ 
