bbe7950751004e46c8bc3636d3da05755adebe80
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-12 17:40:53 +0100

    Modification Routeur, pour prendre en considération les commentaires

--- Controleur/Routeur.php
+++ Controleur/Routeur.php
@@ -1,50 +1,61 @@
 <?php
-
 require_once 'Controleur/ControleurAccueil.php';
 require_once 'Controleur/ControleurBillet.php';
 require_once 'Vue/Vue.php';
 
-class Routeur {
-    
+class Routeur
+{
+
     private $ctrlAccueil;
+
     private $ctrlBillet;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->ctrlAccueil = new ControleurAccueil();
         $this->ctrlBillet = new ControleurBillet();
     }
-    
+
     // Traite une requête entrante
-    public function routerRequete() {
+    public function routerRequete()
+    {
         try {
             if (isset($_GET['action'])) {
                 if ($_GET['action'] == 'billet') {
-                    if (isset($_GET['id'])) {
-                        $idBillet = intval($_GET['id']);
-                        if ($idBillet != 0) {
-                            $this->ctrlBillet->billet($idBillet);
-                        }
-                        else 
-                            throw new Exception("Identifiant de billet non valide");
-                    }
-                    else 
-                        throw new Exception("Identifiant de billet non défini");
-                }
-                else
+                    $idBillet = intval($this->getParametre($_GET, 'id'));
+                    if ($idBillet != 0) {
+                        $this->ctrlBillet->billet($idBillet);
+                    } else
+                        throw new Exception("Identifiant de billet non valide");
+                } else if ($_GET['action'] == 'commenter') {
+                    $auteur = $this->getParametre($_POST, 'auteur');
+                    $contenu = $this->getParametre($_POST, 'contenu');
+                    $idBillet = $this->getParametre($_POST, 'id');
+                    $this->ctrlBillet->commenter($auteur, $contenu, $idBillet);
+                } else
                     throw new Exception("Action non valide");
-            }
-            else { // Aucune action définie : affichage de l'accueil
+            } else { // Aucune action définie : affichage de l'accueil
                 $this->ctrlAccueil->accueil();
             }
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             $this->erreur($e->getMessage());
         }
     }
-    
+
+    private function getParametre($tableau, $nom)
+    {
+        if (isset($tableau[$nom])) {
+            return $tableau[$nom];
+        } else
+            throw new Exception("Paramètre '$nom' absent");
+    }
+
     // Affiche une erreur
-    private function erreur($msgErreur) {
+    private function erreur($msgErreur)
+    {
         $vue = new Vue("Erreurs");
-        $vue->generer(array('msgErreur' => $msgErreur));
+        $vue->generer(array(
+            'msgErreur' => $msgErreur
+        ));
     }
 }
