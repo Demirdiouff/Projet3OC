a42141a150adc2498b7503b49ec1d990fe439035
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-12 19:07:04 +0100

    Modif template général

--- Vue/template.php
+++ Vue/template.php
@@ -1,22 +1,80 @@
 <!doctype html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" href="Contenu/style.css" />
-    <title><?= $titre ?></title>   <!-- Élément spécifique -->
-  </head>
-  <body>
-    <div id="global">
-      <header>
-        <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
-        <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
-      </header>
-      <div id="contenu">
-        <?= $contenu ?>   <!-- Élément spécifique -->
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
+<link rel="stylesheet" href="Contenu/style.css" />
+<title><?= $titre ?></title>
+<!--  $titre se trouve dans vueAccueil.php -->
+<!--  Elément spécifique -->
+<!-- Bootstrap core CSS -->
+<link href="Contenu/bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<link href="Contenu/bootstrap-3.3.7/assets/css/ie10-viewport-bug-workaround.css"
+	rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="Contenu/style.css" rel="stylesheet">
+
+<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="Contenu/bootstrap-3.3.7/assets/js/ie-emulation-modes-warning.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <a href="index.php"><h3 class="masthead-brand">Un billet pour l'Alaska</h3></a>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="index.php">Accueil</a></li>
+                  <li><a href="index.php?action=apropos">A propos</a></li>
+                  <li><a href="index.php?action=roman">Roman</a></li>
+                  <li><a href="index.php?action=contact">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+		<?php echo $contenu; ?>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p><a href="adminPage.php">Espace d'administration</a></p>
+            </div>
+          </div>
+
+        </div>
+
       </div>
-      <footer id="piedBlog">
-        Blog réalisé avec PHP, HTML5 et CSS.
-      </footer>
-    </div> <!-- #global -->
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
