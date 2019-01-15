a38a69dd2907346e9fa2b6a3447bac904f7e1501
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-15 11:50:00 +0100

    Création vueEspaceAdmin

---
+++ Vue/vueEspaceAdmin.php
@@ -0,0 +1,13 @@
+<?php
+ 
+?>
+<p>Connectez vous à votre espace administrateur pour avoir plus de fonctionnalités</p>
+
+<br />
+
+
+<form method="post">
+	<input type="text" id="" name="" placeholder="Pseudo" />
+	<input type="password" id="" name="" placeholder="Mot de passe" />
+	<input type="submit" value="Se connecter" />
+</form>
