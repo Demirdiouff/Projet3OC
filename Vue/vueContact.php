b441ee45b4294f6cd35a80ddfd336cb147b9e498
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-15 11:49:53 +0100

    Création vueContact

---
+++ Vue/vueContact.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<p> Contactez moi ! </p>
+
+<form method="post">
+	<input id="" name="" type="text" placeholder="Nom" />
+	<input id="" name="" type="text" placeholder="Prenom" />
+	<input type="textarea" placeholder="Ecrivez ce que vous souhaitez" size="100px" />
+	<input type="submit" value="Envoyer !" />
+</form>
