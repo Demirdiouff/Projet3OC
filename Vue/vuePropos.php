6efdc46c07e9db6d30e2837c6d2bf7fbf9e5f198
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-15 11:50:07 +0100

    Création vuePropos

---
+++ Vue/vuePropos.php
@@ -0,0 +1,20 @@
+<div class="">
+	<h1>Jean Forteroche</h1>
+	<p class="">Un simple écrivain qui veut faire les choses autrement.</p>
+	<p>
+		<a class="" href="index.php?action=roman" role="">Suivre
+			mes réalisations</a>
+	</p>
+</div>
+
+<div class="">
+	<div class="">
+		<h4>Subheading</h4>
+		<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus
+			mollis interdum.</p>
+
+		<h4>Subheading</h4>
+		<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras
+			mattis consectetur purus sit amet fermentum.</p>
+	</div>
+</div>
