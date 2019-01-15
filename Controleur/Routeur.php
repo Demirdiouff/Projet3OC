54ccc2b26fa9fcb47fa0109f47a438e0bc29ce12
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-01-15 11:49:38 +0100

    Modif Routeur selon les nouveaux Controleurs

--- Controleur/Routeur.php
+++ Controleur/Routeur.php
@@ -1,17 +1,28 @@
 <?php
 require_once 'Controleur/ControleurAccueil.php';
-require_once 'Controleur/ControleurBillet.php';
+require_once 'Controleur/ControleurPropos.php';
 require_once 'Controleur/ControleurRoman.php';
+require_once 'Controleur/ControleurBillet.php';
+require_once 'Controleur/ControleurContact.php';
+require_once 'Controleur/ControleurEspaceAdmin.php';
 require_once 'Vue/Vue.php';
 
 class Routeur
 {
 
     private $ctrlAccueil;
+    
+    private $ctrlPropos;
 
+    private $ctrlRoman;
+    
     private $ctrlBillet;
     
-    private $ctrlRoman;
+    private $ctrlAjoutCommentaireBillet;
+    
+    private $ctrlContact;
+    
+    private $ctrlEspaceAdmin;
 
     public function __construct()
     {
@@ -31,9 +42,35 @@ class Routeur
                     $this->ctrlAccueil = new ControleurAccueil();
                     $this->ctrlAccueil->accueil();
                     break;
+                case 'propos':
+                    $this->ctrlPropos = new ControleurPropos();
+                    $this->ctrlPropos->propos();
                 case 'roman':
                     $this->ctrlRoman = new ControleurRoman();
                     $this->ctrlRoman->roman();
+                    break;
+                case 'billetRoman':
+                    $this->ctrlBillet = new ControleurBillet();
+                    $idBillet = -1;
+                    if (isset($_GET['id'])) {
+                        $idBillet = $_GET['id'];
+                    }
+                    $this->ctrlBillet->billet($idBillet);
+                    break;
+                case 'commenter':
+                    $this->ctrlAjoutCommentaireBillet = new ControleurBillet();
+                    $auteur = $this->getParametre($_POST, 'auteur');
+                    $contenu = $this->getParametre($_POST, 'contenu');
+                    $idBillet = $this->getParametre($_POST, 'id');
+                    $this->ctrlAjoutCommentaireBillet->commenter($auteur, $contenu, $idBillet);
+                    break;
+                case 'contact':
+                    $this->ctrlContact = new ControleurContact();
+                    $this->ctrlContact->contact();
+                    break;
+                case 'espaceAdmin':
+                    $this->ctrlEspaceAdmin = new ControleurEspaceAdmin();
+                    $this->ctrlEspaceAdmin->espaceAdmin();
                     break;
                 // faire quelque chose
                 default:
