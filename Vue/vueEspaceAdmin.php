ab9fe8eda632c7e18d5b822e2c9c1cbf885089a7
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-02-11 22:56:24 +0100

    Modif pour rajouter un formulaire dans la partie connexion

--- Vue/vueEspaceAdmin.php
+++ Vue/vueEspaceAdmin.php
@@ -1,13 +1,27 @@
 <?php
  
 ?>
+
 <p>Connectez vous à votre espace administrateur pour avoir plus de fonctionnalités</p>
+
 
 <br />
 
 
-<form method="post">
-	<input type="text" id="" name="" placeholder="Pseudo" />
-	<input type="password" id="" name="" placeholder="Mot de passe" />
-	<input type="submit" value="Se connecter" />
+<h1>Connexion</h1><br />
+<form method="post" action="index.php?action=connexion">
+	<input type="text" id="txt" name="pseudo" placeholder="Pseudo" required/>
+	<input type="password" id="txt" name="motDePasse" placeholder="Mot de passe" required/>
+	<input id="bouton-default" type="submit" name="connexion" value="Se connecter" />
+</form>
+
+<br />
+<br />
+<p>Ou inscrivez-vous plus bas</p>
+
+<h2>Inscription</h2><br />
+<form method="post" action="index.php?action=inscription">
+	<input type="text" id="txt" name="pseudo" placeholder="Pseudo" required/>
+	<input type="password" id="txt" name="motDePasse" placeholder="Mot de passe" required/>
+	<input id="bouton-default" type="submit" name="inscription" value="S'inscrire" />
 </form>
