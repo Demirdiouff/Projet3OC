2426bbaa98144d49f779f0456691346a9924c67f
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-04-08 16:48:42 +0200

    Création de nouveaux méthodes pour accueillir les nouvelles pages/templates

--- Vue/Vue.php
+++ Vue/Vue.php
@@ -7,9 +7,10 @@ class Vue {
     // Titre de la vue (défini dans le fichier vue)
     private $titre;
     
-    public function __construct($action) {
+    public function __construct($action, $titre = '') {
         // Détermination du nom du fichier vue à partir de l'action
         $this->fichier = "Vue/vue" . $action . ".php";
+        $this->titre = $titre;
     }
     
     // Génère et affiche la vue
@@ -18,6 +19,69 @@ class Vue {
         $contenu = $this->genererFichier($this->fichier, $donnees);
         // Génération ddu gabarit commun utilisant la partie spécifique
         $vue = $this->genererFichier('Vue/template.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageConnexion($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleConnexion.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateConnexion.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageEspaceAdmin($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleConnexion.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateEspaceAdmin.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPagePropos($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et stylePropos.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templatePropos.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageContact($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleContact.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateContact.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageRoman($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleRoman.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateRoman.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageBillet($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleBillet.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateBillet.php', array('titre' => $this->titre, 'contenu' => $contenu));
+        // Renvoi de la vue au navigateur
+        echo $vue;
+    }
+    
+    public function genererPageErreurs($donnees) {
+        // Génération de la partie spécifique de la vue, semblable a la fonction generer mais pour un autre template et styleBillet.css
+        $contenu = $this->genererfichier($this->fichier, $donnees);
+        // Génération du gabarit commun utilisant la partie spécifique
+        $vue = $this->genererfichier('Vue/templateErreurs.php', array('titre' => $this->titre, 'contenu' => $contenu));
         // Renvoi de la vue au navigateur
         echo $vue;
     }
