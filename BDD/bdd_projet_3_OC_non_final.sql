ffefa9fa71881123191e6bf2c1a92551f1ac6722
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-04-08 13:56:39 +0200

    Modification champs BDD pour respecter normes SQL

--- BDD/bdd_projet_3_OC_non_final.sql
+++ BDD/bdd_projet_3_OC_non_final.sql
@@ -17,53 +17,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `T_COMMENTAIRE`
+-- Structure de la table `commentaires`
 --
 
-CREATE TABLE `T_COMMENTAIRE` (
-  `COM_ID` int(11) NOT NULL,
-  `COM_DATE` datetime NOT NULL,
-  `COM_AUTEUR` varchar(100) NOT NULL,
-  `COM_CONTENU` varchar(200) NOT NULL,
-  `BIL_ID` int(11) NOT NULL
+CREATE TABLE `commentaires` (
+  `id_commentaire` int(11) NOT NULL,
+  `date_commentaire` datetime NOT NULL,
+  `auteur_commentaire` varchar(100) NOT NULL,
+  `contenu_commentaire` varchar(200) NOT NULL,
+  `id_post` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `T_COMMENTAIRE`
+-- Déchargement des données de la table `commentaires`
 --
 
-INSERT INTO `T_COMMENTAIRE` (`COM_ID`, `COM_DATE`, `COM_AUTEUR`, `COM_CONTENU`, `BIL_ID`) VALUES
+INSERT INTO `commentaires` (`id_commentaire`, `date_commentaire`, `auteur_commentaire`, `contenu_commentaire`, `id_post`) VALUES
 (1, '2019-01-12 17:37:29', 'A. Nonyme', 'Bravo pour ce début', 1),
 (2, '2019-01-12 17:37:29', 'Moi', 'Merci ! Je vais continuer sur ma lancée', 1),
-(16, '2019-01-12 17:38:47', 'Ded', 'Re-bonjour, c\'est moi !', 1);
+(16, '2019-01-12 17:38:47', 'Ded', 'Re-bonjour, c\'est moi !', 2);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `T_COMMENTAIRE`
+-- Index pour la table `commentaires`
 --
-ALTER TABLE `T_COMMENTAIRE`
-  ADD PRIMARY KEY (`COM_ID`),
-  ADD KEY `fk_com_bil` (`BIL_ID`);
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`id_commentaire`),
+  ADD KEY `ce_com_post` (`id_post`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `T_COMMENTAIRE`
+-- AUTO_INCREMENT pour la table `commentaires`
 --
-ALTER TABLE `T_COMMENTAIRE`
-  MODIFY `COM_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+ALTER TABLE `commentaires`
+  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `T_COMMENTAIRE`
+-- Contraintes pour la table `commentaires`
 --
-ALTER TABLE `T_COMMENTAIRE`
-  ADD CONSTRAINT `fk_com_bil` FOREIGN KEY (`BIL_ID`) REFERENCES `T_BILLET` (`BIL_ID`);
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `ce_com_post` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id_post`);
