236f12e5bbd7760a3ccf0b3a69b1a07719e74060
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2019-04-25 12:57:37 +0200

    Update

--- Modele/AbstractEntity.php
+++ Modele/AbstractEntity.php
@@ -7,8 +7,8 @@ abstract class AbstractEntity
         foreach ($donnees as $cle => $valeur) {
             // On récupère le nom du setter correspondant à l'attribut
             $method = 'set' . implode('', array_map('ucfirst', explode('_', $cle)));
-            //echo 'method : ' . $method . '<br/>';
-            //echo $method . '<br/>';
+//             echo 'method : ' . $method . '<br/>';
+//             echo $method . '<br/>';
             if (method_exists($this, $method)) {
                 // On appelle le setter
                 $this->$method($valeur);
